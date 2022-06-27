754c678929dfb2d876887c35015b702e36dbd341
FahrulID <fahrulofficial@gmail.com>
2022-06-27 22:17:26 +0700

    register

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -54,7 +54,7 @@
                     </div>
                     <label for="password" class="block">
                         <span class="block mb-2 font-medium text-white">Password</span>
-                        <input type="password" id="password" placeholder="Enter your password" minlength="5"
+                        <input type="password" id="password" placeholder="Enter your password" minlength="5" name="password"
                             class="p-2 rounded-md w-full bg-transparent focus:outline-none autofill:bg-transparent focus:border-pink-200 focus:ring-pink-200"
                             @visibility.window="$el.type = ($el.type == 'password') ? 'text' : 'password' ">
                         {{-- <p class="text-xs m-1 text-pink-700 invisible peer-invalid:visible">less than 5 characters</p> --}}
@@ -89,7 +89,7 @@
                     </div>
                     <label for="password" class="block">
                         <span class="block mb-2 font-medium text-white">Repeat Password</span>
-                        <input type="password" id="password" placeholder="Enter your password" minlength="5"
+                        <input type="password" id="password" placeholder="Enter your password" minlength="5" name="password_confirmation"
                             class="p-2 rounded-md w-full bg-transparent focus:outline-none autofill:bg-transparent focus:border-pink-200 focus:ring-pink-200"
                             @visibility.window="$el.type = ($el.type == 'password') ? 'text' : 'password' ">
                         {{-- <p class="text-xs m-1 text-pink-700 invisible peer-invalid:visible">less than 5 characters</p> --}}
