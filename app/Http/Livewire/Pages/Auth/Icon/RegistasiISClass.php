adc667ea29be16fdb7e75c227af1a03db9a74a54
Cizz22 <cisatraa@gmail.com>
2022-06-28 21:08:14 +0700

    add conponents

---
+++ app/Http/Livewire/Pages/Auth/Icon/RegistasiISClass.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Auth\Icon;
+
+use Livewire\Component;
+
+class RegistasiISClass extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.auth.icon.registasi-i-s-class');
+    }
+}
