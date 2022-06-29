07debb7b0f85010e04c58da5e75acb7bfddad94e
Cizz22 <cisatraa@gmail.com>
2022-06-29 18:13:05 +0700

    fix migration

---
+++ app/Models/Icon/IconWebinarKickOff.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Icon;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IconWebinarKickOff extends Model
+{
+    use HasFactory;
+}
