9dec60922b59ea8019b4aac75a2df9c3ab9e1331
Kurniaman <nimanz720@gmail.com>
2022-06-29 23:23:13 +0700

    update icon

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>ISE! 2022</title>
-
+    <!-- linknya -->
     <link rel="icon" href="{{ asset('images/logo-only.png') }}">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <!-- Link Swiper's CSS -->
