022ee4004b03ce52015fc607dc6cda27d6fb2cab
ReyhanDzaki <reyhan.dzaki.a@gmail.com>
2022-06-30 20:24:30 +0700

    feat:bcl responsive

--- resources/views/livewire/pages/landing/bionix.blade.php
+++ resources/views/livewire/pages/landing/bionix.blade.php
@@ -1,12 +1,12 @@
 <div class="bg-black font-poppins">
-    <div class="flex flex-col-reverse lg:flex-row items-start pt-[202px] pl-[130px]">
+    <div class="md:flex md:flex-col-reverse lg:flex-row items-start pt-1 md:pt-[202px] md:pl-[130px]">
         <div class="flex flex-col items-start flex-1 text-white mt-36">
             <h1
-                class="lg:text-7xl text-center lg:!text-left font-bold bg-gradient-to-t bg-clip-text text-transparent from-pink-300 to-[#E499FF]">
+                class="lg:text-7xl ml-8 md:ml-0 text-5xl md:text-center lg:!text-left font-bold bg-gradient-to-t bg-clip-text text-transparent from-pink-300 to-[#E499FF]">
                 Competition <br>
                 <span class="text-white">Zone</span>
             </h1>
-            <div class="relative mt-24 mb-8 text-center lg:px-2 max-w-fit group">
+            <div class="relative mt-16 ml-8 md:ml-0 md:mt-24 md:mb-8 text-center lg:px-2 max-w-fit group">
                 <div class="absolute inset-0 ">
                     <a href="#bsl"
                         class="relative transition duration-1000 opacity-75 btn lg:px-8 blur-lg group-hover:duration-300 group-hover:opacity-100 font-poppins ">Let's
@@ -17,18 +17,18 @@
                 </a>
             </div>
         </div>
-        <div class="flex justify-center flex-1">
+        <div class="ml-48 w-[210px] -translate-y-24 md:-translate-y-0 flex justify-center flex-1">
             <img src="{{ asset('images/isometric-city-bionix.svg') }}" class="w-full h:full">
         </div>
 
     </div>
-    <div x-data="{ tab: 'bsl' }" class="flex-row items-center justify-center text-white ">
+    <div x-data="{ tab: 'bcl' }" class="flex-row items-center justify-center text-white ">
 
-        <div class="flex justify-center gap-4 pt-12">
+        <div class="flex justify-center gap-4 md:pt-12">
             <button :class="{ 'text-white bg-liteBlack': tab === 'bsl', 'text-[#6B6F75]': tab === 'bcl' }"
-                @click.prevent="tab = 'bsl'" class="px-6 py-4 text-base rounded-full ">BIONIX Student Level</button>
+                @click.prevent="tab = 'bsl'" class="px-4 py-3 md:px-6 md:py-4 text-base rounded-full ">BIONIX Student Level</button>
             <button :class="{ 'text-white bg-liteBlack': tab === 'bcl', 'text-[#6B6F75]': tab === 'bsl' }"
-                @click.prevent="tab = 'bcl'" class="px-6 py-4 text-base rounded-full">BIONIX College
+                @click.prevent="tab = 'bcl'" class="px-4 py-3 md:px-6 md:py-4 text-base rounded-full">BIONIX College
                 Level</button>
         </div>
 
@@ -345,17 +345,17 @@
 
         <!-- BCL Start -->
         <div x-show="tab === 'bcl'" class="flex justify-center">
-            <div class="flex-col w-3/4 p-4 text-center font-poppins">
-                <div class="px-[100px] pt-24">
+            <div class="flex-col px-5 md:w-3/4 md:p-4  text-center font-poppins">
+                <div class="md:px-[100px]  pt-24">
                     <h1 class="text-2xl font-bold ">Bionix College Level</h1>
                     <p class="text-lg mt-10  text-[#B5B3BC] text-center">Hadir dengan konsep startup business plan yang
                         akan mengasah skill kreativitas dan problem solving mahasiswa Indonesia dalam mengatasi
                         permasalahan yang ada melalui sebuah ide bisnis kreatif di bidang startup dengan memanfaatkan
                         kemajuan teknologi</p>
                 </div>
-                <div class="flex flex-row justify-center mt-12 space-x-4 font-poppins">
+                <div class="flex flex-col md:flex-row justify-center mt-12 md:space-x-4 font-poppins">
                     <button
-                        class="px-3 py-1 font-poppins bg-[#261A45] text-pink-300 rounded-full flex flex-row items-center  justify-center"><svg
+                        class="md:px-3 md:mx-0 mx-16 py-1 font-poppins bg-[#261A45] text-pink-300 rounded-full flex flex-row items-center justify-center"><svg
                             class="pr-2" width="31" height="32" viewBox="0 0 31 32" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
                             <path d="M10.5034 13.8623L15.4998 18.8506L20.4961 13.8623" stroke="#B221E5"
@@ -368,48 +368,53 @@
                         </svg>
                         Download Guidebook</button>
                     <button
-                        class="px-16 py-1 rounded-full bg-gradient-to-r from-pink-300 to-purple-300">Register</button>
+                        class="my-6 py-2 md:mx-0 mx-28 md:my-0 md:px-16 rounded-full bg-gradient-to-r from-pink-300 to-purple-300">Register</button>
                 </div>
-                <div class="text-6xl text-white font-bold mt-[220px] md:w-[600px] md:mx-auto">
+                <div class="text-4xl md:text-6xl text-white font-bold mt-16 md:mt-[220px] md:w-[600px] md:mx-auto">
                     Many <a class="text-transparent bg-gradient-to-r from-pink-300 to-purple-300 bg-clip-text"> benefit
                     </a> that you can get.
                 </div>
 
-                <div class="mx-auto w-[980px] mt-36  grid grid-cols-6 gap-4">
-                    <div class="bg-gradient-to-r from-purple-75 to-purple-300 h-[330px] rounded-2xl col-span-2  ">
-                        <div class="flex flex-col mx-4 text-2xl text-left pr-18">
+                <div class="mx-auto md:w-[980px] mt-16 md:mt-36 flex flex-col gap-8 md:grid md:grid-cols-6 md:gap-4">
+                    <div class="bg-gradient-to-r from-purple-75 to-purple-300 md:h-[330px] rounded-2xl col-span-2  ">
+                        <div class="flex flex-col md:my-0 my-8 mx-4 md:mx-4 text-2xl text-left pr-18">
 
-                            <a class="mt-8 text-2xl font-bold">Certificate</a>
+                            <a class="md:mt-8 my-2 text-lg md:text-2xl font-bold">E-certificate</a>
+                           
 
-                            <a class="mr-16 ">Daftar dan dapatkan e-certificate</a>
+                            <a class="md:mr-16 text-sm text-[#EAEAEA] md:text-base">Daftar dan dapatkan e-certificate</a>
 
-                            <img class="mx-auto mt-16 w-30" src="{{ asset('images/sertif.svg') }}"
+                            <img class="mx-auto w-20 mt-10 mr-28 md:mx-[80px] md:my-16 md:w-28" src="{{ asset('images/sertif.svg') }}"
                                 alt="purple certificate">
 
                         </div>
                     </div>
-                    <div class="bg-liteBlack h-[330px]  rounded-2xl col-span-4 ">
-                        <div class="flex flex-row mx-4 text-2xl text-left pr-18">
+                    <div class="bg-liteBlack md:h-[330px] md:py-0 py-5 rounded-2xl col-span-4 ">
+                        <div class="flex flex-col md:flex-row mx-4 text-2xl text-left pr-18">
                             <div class="flex flex-col">
-                                <a class="mt-[49px] text-2xl font-bold">Incubation</a>
+                                <a class="md:mt-[49px] text-lg md:text-2xl font-bold">Incubation</a>
 
-                                <a class="text-liteGray w-[272px]">Kesempatan inkubasi dengan perusahaan inkubator yang
+                                <a class="text-liteGray md:text-base md:mr-0 mr-12 text-sm md:w-[272px]">Kesempatan inkubasi dengan perusahaan inkubator yang
                                     bekerja sama dengan ISE!2022</a>
                             </div>
-                            <img class="pt-[121px] ml-[80px]" src="{{ asset('images/workInDesk.png') }}"
+                            <img class="md:block hidden md:pt-[121px] ml-[80px]" src="{{ asset('images/workInDesk.png') }}"
+                                alt="purple certificate">
+                            <img class="md:hidden block w-[250px] h-[250px] ml-16" src="{{ asset('images/m_workInDesk.png') }}"
                                 alt="purple certificate">
 
                         </div>
                     </div>
-                    <div class=" bg-gradient-to-t from-pink-300 to-purple-300  h-[330px] rounded-2xl col-span-6">
-                        <div class="flex flex-row mx-4 text-2xl text-left pr-18">
-                            <div class="flex flex-col ml-8 ">
-                                <a class="mt-[49px] text-2xl font-bold">Mentoring</a>
+                    <div class=" bg-gradient-to-t from-pink-300 to-purple-300 md:py-0 py-5 md:h-[330px] rounded-2xl col-span-6">
+                        <div class="flex flex-col md:flex-row mx-4 text-2xl text-left md:pr-18">
+                            <div class="flex flex-col md:ml-8 ">
+                                <a class="md:mt-[49px] text-lg md:text-2xl text-[#FFFFFF] font-bold">Mentoring</a>
 
-                                <a class="text-liteGray w-[501px] h-[114] mt-2">terkait ide bisnis yang dikembangkan
+                                <a class="text-liteGray text-sm md:text-base md:w-[501px] md:h-[114] mt-2">terkait ide bisnis yang dikembangkan
                                     bersama dengan pemateri yang kompeten pada bidang bisnis dan teknologi informasi</a>
                             </div>
-                            <img class="pt-[63px] ml-[70px]" src="{{ asset('images/present.svg') }}"
+                            <img class="md:block hidden md:pt-[63px] md:ml-[70px]" src="{{ asset('images/present.svg') }}"
+                                alt="purple certificate">
+                            <img class="md:hidden block w-[300px] h-[300px] ml-4" src="{{ asset('images/m_present.svg') }}"
                                 alt="purple certificate">
 
                         </div>
@@ -420,7 +425,7 @@
 
                 </div>
 
-                <div class="text-6xl text-white font-bold mt-36 md:w-[537px] md:mx-auto ">
+                <div class="text-4xl md:text-6xl text-white font-bold mt-36 md:w-[537px] md:mx-auto ">
                     Join and earn <a
                         class="text-transparent bg-gradient-to-r from-pink-300 to-purple-300 bg-clip-text"> fantastic
                     </a> prizes
@@ -428,16 +433,16 @@
                 
                 <div class="relative font-poppins">
                     <img class="left-0 right-0 mx-auto" src="{{ asset('images/trophy.svg') }}" alt="">
-                    <h1 class="absolute text-2xl transform -translate-x-1/2 -translate-y-1/2 top-3/4 left-1/2">Total
+                    <h1 class="absolute md:text-2xl text-xs transform -translate-x-1/2 -translate-y-1/2 top-3/4 left-1/2">Total
                         Prizes</h1>
-                    <h1 class="absolute mt-4 text-3xl font-bold transform -translate-x-1/2 top-3/4 left-1/2 ">Up to Rp
+                    <h1 class="absolute mt-4 w-64 md:w-96 text-xl md:text-3xl font-bold transform -translate-x-1/2 top-3/4 left-1/2 ">Up to Rp
                         10.000.000</h1>
                 </div>
 
 
                 <!-- Timeline Start -->
                 <div class="my-36">
-                    <h1 class="mb-24 font-bold text-white font-poppins text-7xl">Mark your calender and <br> follow the
+                    <h1 class="mb-24 font-bold text-white font-poppins text-5xl md:text-7xl">Mark your calender and <br> follow the
                         <span
                             class="text-transparent bg-gradient bg-gradient-to-r from-pink-300 to-purple-300 bg-clip-text">excitement</span>
                     </h1>
