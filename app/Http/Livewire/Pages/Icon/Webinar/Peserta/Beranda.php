17d93eda37ea68e6050835eda4831b9c37ffab0e
Cizz22 <cisatraa@gmail.com>
2022-07-16 09:07:26 +0700

    fix:webinar alert message

--- app/Http/Livewire/Pages/Icon/Webinar/Peserta/Beranda.php
+++ app/Http/Livewire/Pages/Icon/Webinar/Peserta/Beranda.php
@@ -27,9 +27,9 @@ class Beranda extends Component
     {
         $this->alert_color = 'blue';
 
-        $this->alert_content = $this->is_event_today ? 'presensi' :'Informasi link zoom akan diberikan tanggal <b>13 Juli 2022</b> melalui laman website ISE 2022 dan Email peserta yang telah didaftarkan.';
+        $this->alert_content = $this->is_event_today ? 'Hi Peserta Webinar Kick-Off ISE! 2022, Join Zoom menggunakan link yang tersedia dan jangan lupa mengisi form presensi. Di akhir acara luangkan waktumu untuk mengisi feedback Webinar Kick-off ISE! 2022, Saran dan kritikmu sangat membantu untuk kami. Selamat menikmati Webinar Kick-Off ISE! 2022 ' :'Informasi link zoom akan diberikan tanggal <b>13 Juli 2022</b> melalui laman website ISE 2022 dan Email peserta yang telah didaftarkan.';
 
-        $this->alert_header =  'Link Zoom Webinar Kick Off 2022';
+        $this->alert_header =  $this->is_event_today ? 'D-Day!' : "Link Zoom Webinar Kick Off 2022";
     }
 
     public function closeNotif(){
