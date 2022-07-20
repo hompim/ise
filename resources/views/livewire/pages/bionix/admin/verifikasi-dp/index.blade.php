0d7ae2e7ba1c8b3b657d237563512ee6ca90ee3e
Cizz22 <cisatraa@gmail.com>
2022-07-20 21:07:36 +0700

    fix dashboard

--- resources/views/livewire/pages/bionix/admin/verifikasi-dp/index.blade.php
+++ resources/views/livewire/pages/bionix/admin/verifikasi-dp/index.blade.php
@@ -1,3 +1,18 @@
-<div>
-    {{-- Success is as dangerous as failure. --}}
+<div class="px-8 text-white">
+    <h3 class="text-2xl font-weight-bold">Promo</h3>
+    <div class="card rounded-xl mt-4" style="background-color: #191a1e; border:0">
+        <div class="card-body">
+            <div class="flex">
+                <button
+                    wire:click="$emit('openModal', 'pages.bionix.admin.promo.components.moda-add-edit',{{json_encode(['type'=>'add'])}})"
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full self-center">
+                    Add
+                </button>
+            </div>
+            <livewire:pages.bionix.admin.verifikasi-dp.datatable.index/>
+        </div>
+    </div>
 </div>
+@push('js')
+    @livewire('livewire-ui-modal')
+@endpush
