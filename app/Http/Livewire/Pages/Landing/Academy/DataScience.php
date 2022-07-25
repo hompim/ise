39e8ae79191492c6195401776df7deca93e538fb
Cizz22 <cisatraa@gmail.com>
2022-07-25 15:25:21 +0700

    add landing icon compoenents

---
+++ app/Http/Livewire/Pages/Landing/Academy/DataScience.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Landing\Academy;
+
+use Livewire\Component;
+
+class DataScience extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.landing.academy.data-science');
+    }
+}
