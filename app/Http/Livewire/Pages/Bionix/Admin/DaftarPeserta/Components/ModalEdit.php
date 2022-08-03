7dffa059a66672bc3f2c05103f616f0decff1070
Cizz22 <cisatraa@gmail.com>
2022-08-03 15:35:23 +0700

    fix dashboard bionix

--- app/Http/Livewire/Pages/Bionix/Admin/DaftarPeserta/Components/ModalEdit.php
+++ app/Http/Livewire/Pages/Bionix/Admin/DaftarPeserta/Components/ModalEdit.php
@@ -13,6 +13,9 @@ class ModalEdit extends ModalComponent
     public $anggota_1;
     public $anggota_2;
     public $anggota_3;
+    public $anggota_1_email;
+    public $anggota_2_email;
+    public $anggota_3_email;
     public $nama_institusi;
     public $type;
 
@@ -23,6 +26,9 @@ class ModalEdit extends ModalComponent
         $this->anggota_1 = $this->bionix_data->leader->name;
         $this->anggota_2 = $this->type == ' student' ? ($this->bionix_data->member_id ? $this->bionix_data->member->name : null) : ($this->bionix_data->member1_id ? $this->bionix_data->member_1->name : null);
         $this->anggota_3 = $this->type == ' student' ? null : ($this->bionix_data->member2_id ? $this->bionix_data->member_2->name : null);
+        $this->anggota_1_email = $this->bionix_data->leader->email;
+        $this->anggota_2_email = $this->type == ' student' ? ($this->bionix_data->member_id ? $this->bionix_data->member->email : null) : ($this->bionix_data->member1_id ? $this->bionix_data->member_1->email : null);
+        $this->anggota_3_email = $this->type == ' student' ? null : ($this->bionix_data->member2_id ? $this->bionix_data->member_2->email : null);
         $this->nama_institusi = $this->type == ' student' ? $this->bionix_data->school_name : $this->bionix_data->university_name;
     }
 
@@ -31,16 +37,20 @@ class ModalEdit extends ModalComponent
         if ($this->type == 'student') {
             $this->validate([
                 'anggota_1' => 'required|string',
-                'nama_institusi' => 'required|string'
+                'anggota_1_email' => 'required|string',
+                'nama_institusi' => 'required|string|email'
             ]);
             if ($this->bionix_data->member_id) {
                 $this->validate([
-                    'anggota_2' => 'required|string'
+                    'anggota_2' => 'required|string',
+                    'anggota_2_email' => 'required|string|email'
                 ]);
                 $this->bionix_data->member->name = $this->anggota_2;
+                $this->bionix_data->member->email = $this->anggota_2_email;
                 $this->bionix_data->member->save();
             }
             $this->bionix_data->leader->name = $this->anggota_1;
+            $this->bionix_data->leader->email = $this->anggota_1_email;
             $this->bionix_data->leader->save();
             $this->bionix_data->school_name = $this->nama_institusi;
             $this->bionix_data->save();
--- resources/views/livewire/pages/bionix/admin/daftar-peserta/components/modal-edit.blade.php
+++ resources/views/livewire/pages/bionix/admin/daftar-peserta/components/modal-edit.blade.php
@@ -1,12 +1,11 @@
 <div class="p-4">
     <div class="flex justify-between">
         <h5 class="text-lg font-weight-bold">Data Peserta</h5>
-        <button type="button" title="Tutup" wire:click="closeModal()" class="self-start"><i
-                class="cil-x"></i></button>
+        <button type="button" title="Tutup" wire:click="closeModal()" class="self-start"><i class="cil-x"></i></button>
     </div>
-    <hr/>
+    <hr />
     <form class="mt-4 w-full max-w-full" wire:submit.prevent="submit" autocomplete="off">
-        @if($errors->any())
+        @if ($errors->any())
             <div class="bg-red-400 border-l-4 border-red-500 p-4 mb-3" role="alert">
                 <p class="font-bold text-lg">Validation Error</p>
                 @foreach ($errors->all() as $error)
@@ -23,24 +22,42 @@
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-payer">
                     Nama Anggota 1
                 </label>
-                <input
+                <input name="anggota_1"
                     class="appearance-none block w-full bg-white text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                     id="anggota_1" type="text" required wire:model.defer="anggota_1" required>
             </div>
-            @if(($type=='student'&&$this->bionix_data->member_id)||($type=='college'&&$this->bionix_data->member1_id))
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-payer">
+                    Email Anggota 1
+                </label>
+                <input name="anggota_1_email"
+                    class="appearance-none block w-full bg-white text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                    id="anggota_1" disabled type="email" required wire:model.defer="anggota_1_email" required>
+            </div>
+            @if (($type == 'student' && $this->bionix_data->member_id) ||
+                ($type == 'college' && $this->bionix_data->member1_id))
                 <div class="w-full md:w-1/2 px-3">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                           for="grid-creator">
+                        for="grid-creator">
                         Nama Anggota 2
                     </label>
-                    <input
+                    <input name="anggota_2"
                         class="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                         id="anggota_1" type="text" required wire:model.defer="anggota_2" required>
+                </div>
+                <div class="w-full md:w-1/2 px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                        for="grid-creator">
+                        Email Anggota 2
+                    </label>
+                    <input name="anggota_2_email"
+                        class="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id="anggota_2_email" type="email" required wire:model.defer="anggota_2_email" required>
                 </div>
             @endif
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
-            @if($type=='college'&&$this->bionix_data->member2_id)
+            @if ($type == 'college' && $this->bionix_data->member2_id)
                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-bank">
                         Nama Anggota 3
@@ -66,4 +83,3 @@
         </div>
     </form>
 </div>
-
