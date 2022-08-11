39679af3bbb73e161979e5d2d887d9b067296e8a
Kurniaman Andreas Zega <nimanz720@gmail.com>
2022-08-11 18:03:59 +0700

    update: prestasi & true-false page

--- resources/views/livewire/pages/landing/e-hall/prestasi/index.blade.php
+++ resources/views/livewire/pages/landing/e-hall/prestasi/index.blade.php
@@ -349,3 +349,5 @@
 <!-- carousel prestasi -->
 
 
+
+
--- resources/views/livewire/pages/landing/e-hall/quiz/true-or-false.blade.php
+++ resources/views/livewire/pages/landing/e-hall/quiz/true-or-false.blade.php
@@ -1,3 +1,130 @@
-<div>
-    {{-- Be like water. --}}
+
+<div class="pt-6 md:pt-52 flex flex-col bg-black">
+    <h1 class="mx-auto text-[27px] md:text-7xl font-bold text-white">Startup</h1>
+    {{-- Start Quiz Block --}}
+    <div class="mt-32 mb-[145px] bg-liteBlack mx-6 md:mx-28 h-max rounded-2xl flex flex-col p-11">
+        <h1 class="text-white text-lg md:text-2xl mx-auto">Quiz (nama startup)</h1>
+        <div class="flex flex-col md:flex-row mt-4 gap-14">
+            {{-- Left Part --}}
+            <div class="flex flex-col">
+                <h2 class="text-white text-lg md:text-2xl underline underline-offset-4 decoration-purple-300">Quiz 2</h2>
+                <p class="text-sm md:text-lg text-liteGray mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum
+                    est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan.
+                </p>
+                <div class="flex flex-row mt-10 mx-auto gap-8 md:gap-14">
+                    <button class="bg-[#19AC66] w-[109px] md:w-[171px] h-[37px] md:h-[58px] rounded-lg text-white text-sm md:text-xl">True</button>
+                    <button class="bg-[#DE4343] w-[109px] md:w-[171px] h-[37px] md:h-[58px] rounded-lg text-white text-sm md:text-xl">False</button>
+                </div>
+                <div class="md:flex flex-row gap-7 mt-32 hidden">
+                    <button class="bg-[#2F2F2F] w-[58px] h-[58px] text-white items-center rounded-lg">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+                        </svg>
+                    </button>
+                    <button class="bg-[#2F2F2F] w-[58px] h-[58px] text-white items-center rounded-lg">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            {{-- Right Part --}}
+            <div class="flex flex-col ml-auto">
+                <div class="grid grid-cols-2 gap-4 md:px-0 px-12">
+                    <button class="w-[56px] md:w-[75px] h-[56px] md:h-[75px] bg-[#2F2F2F] mx-auto text-white text-2xl font-bold rounded-lg">1</button>
+                    <button class="w-[56px] md:w-[75px] h-[56px] md:h-[75px] bg-[#2F2F2F] mx-auto text-white text-2xl font-bold rounded-lg">2</button>
+                    <button class="w-[56px] md:w-[75px] h-[56px] md:h-[75px] bg-[#2F2F2F] mx-auto text-white text-2xl font-bold rounded-lg">3</button>
+                    <button class="w-[56px] md:w-[75px] h-[56px] md:h-[75px] bg-[#2F2F2F] mx-auto text-white text-2xl font-bold rounded-lg">4</button>
+                </div>
+                <button class="mx-auto text-white md:text-xl rounded-lg col-span-2 btn px-8 md:px-10 py-2 mt-8 md:mt-40 font-semibold">Submit</button>
+                <div class="flex flex-row gap-7 mx-auto mt-12 md:hidden">
+                    <button class="bg-[#2F2F2F] w-[35px] h-[35px] text-white items-center rounded-lg">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+                        </svg>
+                    </button>
+                    <button class="bg-[#2F2F2F] w-[35px] h-[35px] text-white items-center rounded-lg">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
 </div>
+
+<!-- pembahasan quiz -->
+<div class="pt-6 md:pt-52 flex flex-col bg-black">
+    <h1 class=" mx-auto text-[27px] md:text-7xl font-bold text-white">Startup</h1>
+    {{-- Start Quiz Block --}}
+    <div class=" mt-32 mb-[145px] bg-liteBlack mx-6 md:mx-28 rounded-2xl flex flex-col p-11 h-max">
+        <h1 class="text-white text-lg md:text-2xl mx-auto">Quiz (nama startup)</h1>
+        <div class="flex flex-col md:flex-row mt-4 gap-14">
+            {{-- Left Part --}}
+            <div class="flex flex-col">
+                <h2 class="text-white text-lg md:text-2xl underline underline-offset-4 decoration-purple-300">Quiz 2</h2>
+                <p class="text-sm md:text-lg text-liteGray mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum
+                    est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan.
+                </p>
+                <div class="flex flex-row mt-10 mx-auto gap-8 md:gap-14">
+                    <button class="bg-[#19AC66] w-[109px] md:w-[171px] h-[37px] md:h-[58px] rounded-lg text-white text-sm md:text-xl">True</button>
+                    <button class="bg-[#DE4343] w-[109px] md:w-[171px] h-[37px] md:h-[58px] rounded-lg text-white text-sm md:text-xl">False</button>
+                </div>
+                <div class="md:flex flex-row gap-7 mt-32 hidden">
+                    <button class="bg-[#2F2F2F] w-[58px] h-[58px] text-white items-center rounded-lg">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+                        </svg>
+                    </button>
+                    <button class="bg-[#2F2F2F] w-[58px] h-[58px] text-white items-center rounded-lg">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            {{-- Right Part --}}
+            <div class="flex flex-col ml-auto">
+                <div class="grid grid-cols-2 gap-4 md:px-0 px-12">
+                    <button class="w-[56px] md:w-[75px] h-[56px] md:h-[75px] bg-[#2F2F2F] mx-auto text-white text-2xl font-bold rounded-lg">1</button>
+                    <button class="w-[56px] md:w-[75px] h-[56px] md:h-[75px] bg-[#2F2F2F] mx-auto text-white text-2xl font-bold rounded-lg">2</button>
+                    <button class="w-[56px] md:w-[75px] h-[56px] md:h-[75px] bg-[#2F2F2F] mx-auto text-white text-2xl font-bold rounded-lg">3</button>
+                    <button class="w-[56px] md:w-[75px] h-[56px] md:h-[75px] bg-[#2F2F2F] mx-auto text-white text-2xl font-bold rounded-lg">4</button>
+                </div>
+                <button class="mx-auto text-white md:text-xl rounded-lg col-span-2 btn px-8 md:px-10 py-2 mt-8 md:mt-40 font-semibold">Submit</button>
+                <div class="flex flex-row gap-7 mx-auto mt-12 md:hidden">
+                    <button class="bg-[#2F2F2F] w-[35px] h-[35px] text-white items-center rounded-lg">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+                        </svg>
+                    </button>
+                    <button class="bg-[#2F2F2F] w-[35px] h-[35px] text-white items-center rounded-lg">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+
+
+        </div>
+        <!-- start: pembahasan-->
+        <div class="divide-y-[1px] mt-[55px] divide-white">
+            <h1 class="text-white"></h1>
+            <h1 class="text-white pt-[29px] text-[20px] ">Jawaban :
+                <br><span class="text-[#B5B3BC] font-bold text-[24px]">True</span>
+                <br><span class="text-white text-[20px] ">Pembahasan :</span>
+                <br><span class="text-[#B5B3BC] text-[20px]">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a,  mattis tellus. Sed
+                    dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas
+                    eget condimentum velit, sit amet feugiat lectus. Class aptent taciti  sociosqu ad litora torquent per conubia nostra
+                </span>
+            </h1>
+        </div>
+        <!-- end: pembahasan-->
+    </div>
+</div>
+
