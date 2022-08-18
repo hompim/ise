32eccdf5d3e89465facad06b10c2e0baedfd55ba
Muhammad Ridho Waradana <muhammadridhowaradana@gmail.com>
2022-08-18 13:11:54 +0700

    feat: responsive modal myskill icon dashboard

--- resources/views/livewire/components/modal-myskill-icon.blade.php
+++ resources/views/livewire/components/modal-myskill-icon.blade.php
@@ -1,16 +1,16 @@
 <div
-    class="fixed left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 flex justify-center items-center bg-black bg-opacity-75 w-screen h-screen">
-    <div class="bg-liteBlack w-[450px] h-[466px] flex flex-col space-y-4 rounded-3xl p-4">
+    class="fixed left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 flex justify-center items-center bg-black w-screen h-screen">
+    <div class="bg-liteBlack md:w-[450px] md:h-[466px] mx-4 flex flex-col space-y-4 rounded-3xl p-4">
         <div class="w-12 text-center flex justify-center mx-auto">
             <img src="{{ asset('images/modal-myskill/present.svg') }}" alt="present" class="w-full">
         </div>
-        <p class="text-center text-lg leading-loose">
+        <p class="text-center text-base md:text-lg leading-loose">
             Yeay! Kamu mendapatkan
             <strong>diskon 25%</strong> untuk pembelian video
             e-learning MySkill dengan minimal pembelian Rp93.000
         </p>
         <div class="text-center">
-            <p class="text-lg">
+            <p class="text-base md:text-lg">
                 Kode kupon Kamu
             </p>
             <div class="relative left-0 right-0 justify-center mx-auto mt-4 text-center max-w-fit group"
@@ -26,7 +26,7 @@
         </div>
         <div class="mx-auto pt-10">
             <a href="https://myskill.id/"
-                class="border border-white text-xl font-bold rounded-full py-3 px-6 text-center hover:opacity-100 opacity-70 hover:text-white  hover:no-underline transition duration-300">
+                class="border border-white text-sm md:text-xl font-bold rounded-full py-3 px-6 text-center hover:opacity-100 opacity-70 hover:text-white  hover:no-underline transition duration-300">
                 Pergi ke Website MySkill
             </a>
         </div>
