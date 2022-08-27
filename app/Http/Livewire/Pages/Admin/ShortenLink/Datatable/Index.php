e8473b80fcf528b95bc0d7bd058d908e284ab35a
Cizz22 <cisatraa@gmail.com>
2022-08-28 06:14:06 +0700

    shorten link

--- app/Http/Livewire/Pages/Admin/ShortenLink/Datatable/Index.php
+++ app/Http/Livewire/Pages/Admin/ShortenLink/Datatable/Index.php
@@ -16,8 +16,8 @@ class Index extends LivewireDatatable
         Column::name('shorten_link')->label('Link'),
         Column::name('destination')->label('Tujuan')->searchable(),
         Column::name('description')->label('Deskripsi')->searchable(),
-        Column::callback(['qr_code'], function ($qr_code) {
-            return view('livewire.pages.admin.shorten-link.components.datatable-qr_code', ['qr_code' => $qr_code]);
+        Column::callback(['shorten_link'], function ($shorten_link) {
+            return view('livewire.pages.admin.shorten-link.components.datatable-qr_code', ['shorten_link' => $shorten_link]);
         })->label("QR Code"),
         Column::callback(['id'], function ($id) {
             return view('livewire.pages.admin.shorten-link.components.datatable-actions', ['id' => $id]);
