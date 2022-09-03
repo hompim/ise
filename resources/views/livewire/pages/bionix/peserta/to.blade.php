d0a4e29b0071c5dd245c5985be133321421e8312
Cizz22 <cisatraa@gmail.com>
2022-09-03 07:57:13 +0700

    fix to page

--- resources/views/livewire/pages/bionix/peserta/to.blade.php
+++ resources/views/livewire/pages/bionix/peserta/to.blade.php
@@ -1,9 +1,9 @@
-<div class="px-8 text-white">
+<div class="px-8">
     <h3 class="text-xl font-weight-bold my-4">Try Out</h3>
     <div class="grid grid-cols-1 gap-6 mt-4 mb-4">
         <div>
 
-            <div class="p-4 rounded-xl shadow-md border-0" style="background-color: #191a1e">
+            <div class="p-4 bg-white rounded-xl shadow-md">
                 <div>
                     <h2
                         class="font-bold text-lg text-gray-600 capitalize">Jadwal Try Out</h2>
@@ -15,8 +15,8 @@
                     </ol>
                     <h2>Berikut beberapa panduan yang bisa anda ikuti bisa mengalami kesulitan</h2>
                     <ol>
-                        <li><a href="https://drive.google.com/file/d/1OqaKMaVQlLS0xPCh1Mpe5Vi5VxTGJEW1/view?usp=sharing">Guide Book Student</a></li>
-                        <li><a href="https://scribehow.com/shared/Bionix_Competition_Tryout__V_ik-HpLRK-vGUfpb2YGRA">Panduan penggunaan website TO</a></li>
+                        <li><a class="text-blue-500" href="https://drive.google.com/file/d/1OqaKMaVQlLS0xPCh1Mpe5Vi5VxTGJEW1/view?usp=sharing">Guide Book Student</a></li>
+                        <li><a class="text-blue-500" href="https://scribehow.com/shared/Bionix_Competition_Tryout__V_ik-HpLRK-vGUfpb2YGRA">Panduan penggunaan website</a></li>
                     </ol>
                     <h2
                         class="font-bold text-lg mt-4 text-gray-600 capitalize">Anda dapat menggunakan tombol di bawah
@@ -26,7 +26,7 @@
                         <a href="https://www.competition.ise-its.com" target="_blank">
                             <button
                                 type="button"
-                                class="bg-purple-400 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mx-2">
+                                class="bg-red-400 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mx-2">
                                 Menuju Website Try Out
                             </button>
                         </a>
