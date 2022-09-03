67a26ec6735628c8085b08cfcf47e7b6685f74be
Cizz22 <cisatraa@gmail.com>
2022-09-03 16:09:33 +0700

    disable edit

--- resources/views/livewire/pages/bionix/admin/daftar-peserta/components/datatable-action.blade.php
+++ resources/views/livewire/pages/bionix/admin/daftar-peserta/components/datatable-action.blade.php
@@ -1,10 +1,5 @@
 <div class="flex space-x-1 justify-around">
     <button
-        onclick="Livewire.emit('openModal', 'pages.bionix.admin.daftar-peserta.components.moda-detail',{{json_encode(['type'=>$type,'id'=>$id])}})"
-        class="p-1 text-blue-400 hover:bg-blue-600 hover:text-white rounded" title="Lihat Detail">
-        <i class="cil-search"></i>
-    </button>
-    <button
         onclick="Livewire.emit('openModal', 'pages.bionix.admin.daftar-peserta.components.modal-edit',{{json_encode(['type'=>$type,'id'=>$id])}})"
         class="p-1 text-green-400 hover:bg-green-600 hover:text-white rounded" title="Edit">
         <i class="cil-pencil"></i>
