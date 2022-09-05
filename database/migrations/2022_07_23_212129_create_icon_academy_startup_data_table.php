b5b42b93ef3569a916cc9896e1c2df6c47be0d54
Cizz22 <cisatraa@gmail.com>
2022-09-05 18:03:01 +0700

    fix typo

--- database/migrations/2022_07_23_212129_create_icon_academy_startup_data_table.php
+++ database/migrations/2022_07_23_212129_create_icon_academy_startup_data_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->text('startup_idea_desc');
             $table->enum('commitement_payment_status', ['Belum Unggah', 'Tahap Verifikasi', 'Terverifikasi', 'Ditolak'])->default('Belum Unggah');
             $table->string('commitement_payment_path')->nullable();
-            $table->enum('competition_round', ['Administrasi', 'Pending','Commitment Fee' ,'Penyisihan', 'Semifinal', 'Final', 'Rejected'])->default('Administrasi');
+            $table->enum('competition_round', ['Administrasi', 'Pending','Commitement Fee' ,'Penyisihan', 'Semifinal', 'Final', 'Rejected'])->default('Administrasi');
             $table->text('commitement_payment_comment')->nullable();
             $table->unsignedBigInteger('leader_id');
             $table->unsignedBigInteger('member1_id');
