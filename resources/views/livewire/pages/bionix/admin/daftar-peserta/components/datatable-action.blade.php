64cbb9c18d71076efc30a4f7f3c2e306baf3746e
Cizz22 <cisatraa@gmail.com>
2022-09-09 19:11:01 +0700

    fix bionix admin

--- resources/views/livewire/pages/bionix/admin/daftar-peserta/components/datatable-action.blade.php
+++ resources/views/livewire/pages/bionix/admin/daftar-peserta/components/datatable-action.blade.php
@@ -4,4 +4,9 @@
         class="p-1 text-green-400 hover:bg-green-600 hover:text-white rounded" title="Edit">
         <i class="cil-pencil"></i>
     </button>
+    <button
+    onclick="Livewire.emit('openModal', 'pages.bionix.admin.daftar-peserta.components.moda-detail',{{ json_encode(['id' => $id,'type'=>$type]) }})"
+    class="p-1 text-blue-600 hover:bg-blue-600 hover:text-white rounded" title="Lihat Detail">
+    <i class="cil-search"></i>
+</button>
 </div>
