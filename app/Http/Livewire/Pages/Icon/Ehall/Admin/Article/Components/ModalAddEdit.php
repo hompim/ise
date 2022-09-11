2d3cdf1ddc8a25581ecc6a37a2df7eb88df35ba2
Cizz22 <cisatraa@gmail.com>
2022-09-11 12:39:48 +0700

    add edit artikel

--- app/Http/Livewire/Pages/Icon/Ehall/Admin/Article/Components/ModalAddEdit.php
+++ app/Http/Livewire/Pages/Icon/Ehall/Admin/Article/Components/ModalAddEdit.php
@@ -26,6 +26,7 @@ class ModalAddEdit extends ModalComponent
     public $logo;
     public $title_image;
     public $images = [];
+    public $isImageChange;
 
 
     public function mount($type, $id = null)
@@ -37,6 +38,9 @@ class ModalAddEdit extends ModalComponent
             $this->subtitle = $this->artikel->subtitle;
             $this->article_type = $this->artikel->type;
             $this->content = $this->artikel->content;
+            $this->link_youtube = $this->artikel->link_youtube;
+            $this->images = $this->artikel->galery;
+            $this->isImageChange = false;
         }
     }
 
@@ -48,11 +52,11 @@ class ModalAddEdit extends ModalComponent
             'content' => 'required'
         ]);
 
-        $this->validate([
-            'images.*' => 'image|max:2048', // 1MB Max
-        ]);
-
         if ($this->modal_type == 'add') {
+
+            $this->validate([
+                'images.*' => 'image|max:2048', // 1MB Max
+            ]);
             $article = EhallArticle::create([
                 'title' => $this->title,
                 'subtitle' => $this->subtitle,
@@ -123,18 +127,101 @@ class ModalAddEdit extends ModalComponent
                     'image_path' => 'ehall/' . $name
                 ]);
             }
-        } elseif ($this->type == 'edit') {
-            $this->promo->update([
+        } elseif ($this->modal_type == 'edit') {
+            if ($this->isImageChange) {
+                $this->validate([
+                    'images.*' => 'image|max:2048', // 1MB Max
+                ]);
+            }
+
+            $this->artikel->update([
                 'title' => $this->title,
                 'subtitle' => $this->subtitle,
-                'type' => $this->type,
+                'type' => $this->article_type,
                 'content' => $this->content,
                 'slug' => Str::slug($this->title),
+                'link_youtube' => $this->link_youtube
             ]);
+
+            if ($this->logo) {
+                $name = date('YmdHis') . '_LOGO_' . $this->title . '.' . $this->logo->getClientOriginalExtension();
+                $resized_image = (new ImageManager())
+                    ->make($this->logo)
+                    ->resize(600, null, function ($constraint) {
+                        $constraint->aspectRatio();
+                        $constraint->upsize();
+                    })->encode($this->logo->getClientOriginalExtension());
+
+                Storage::disk('public')
+                    ->put(
+                        'ehall/' . $name,
+                        $resized_image->__toString()
+                    );
+
+                $this->artikel->update([
+                    'logo_image_path' => 'ehall/' . $name
+                ]);
+            }
+
+            if ($this->title_image) {
+                $name = date('YmdHis') . '_TITLE_' . $this->title . '.' . $this->title_image->getClientOriginalExtension();
+                $resized_image = (new ImageManager())
+                    ->make($this->title_image)
+                    ->resize(600, null, function ($constraint) {
+                        $constraint->aspectRatio();
+                        $constraint->upsize();
+                    })->encode($this->title_image->getClientOriginalExtension());
+
+                Storage::disk('public')
+                    ->put(
+                        'ehall/' . $name,
+                        $resized_image->__toString()
+                    );
+
+                $this->artikel->update([
+                    'title_image_path' => 'ehall/' . $name
+                ]);
+            }
+
+            if ($this->isImageChange) {
+
+                foreach ($this->images as $key => $image) {
+                    $name = date('YmdHis') . '_GALERY_' . $this->title . '_' . $key . '.' . $image->getClientOriginalExtension();
+                    $resized_image = (new ImageManager())
+                        ->make($image)
+                        ->resize(600, null, function ($constraint) {
+                            $constraint->aspectRatio();
+                            $constraint->upsize();
+                        })->encode($image->getClientOriginalExtension());
+
+                    Storage::disk('public')
+                        ->put(
+                            'ehall/' . $name,
+                            $resized_image->__toString()
+                        );
+
+                    EhallArticleImage::create([
+                        'article_id' => $this->artikel->id,
+                        'image_path' => 'ehall/' . $name
+                    ]);
+                }
+            }
         }
 
         $this->emit('refreshLivewireDatatable');
         $this->closeModal();
+    }
+
+    public function deleteImage()
+    {
+        if ($this->modal_type == 'edit') {
+            foreach ($this->artikel->galery as $key => $g) {
+                $g->delete();
+            }
+        }
+
+        $this->isImageChange = true;
+        $this->images = [];
     }
 
 
