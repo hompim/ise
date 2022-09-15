daea9579b7c88f38cec2698f52f6fd46357f8438
MrWaradana <muhammadridhowaradana@gmail.com>
2022-09-15 21:43:12 +0700

    fix: front-end quiz, Pilgan & ToF

--- resources/views/livewire/pages/landing/e-hall/components/quiz-card.blade.php
+++ resources/views/livewire/pages/landing/e-hall/components/quiz-card.blade.php
@@ -7,7 +7,7 @@
             <h1 class="text-white text-lg  md:text-2xl mx-auto">Quiz {{ $type_quiz->name }}</h1>
             <div class="flex flex-col md:flex-row mt-4 gap-14">
                 {{-- Left Part --}}
-                <div class="flex flex-col">
+                <div class="flex flex-col w-full lg:w-1/2">
                     <h2 class="text-white">
                         @if (Auth::check() && Auth::user()->userable_type == 'App\Models\Member')
                             <span class="underline text-xl underline-offset-4 decoration-purple-300 ">Score :
@@ -16,10 +16,13 @@
                         <span class="underline text-2xl underline-offset-4 decoration-purple-300 ">Quiz
                             {{ $quizNo + 1 }}</span>
                         <span class="text-lg ">
-                            <span class="text-[#B221E5]">+10</span>
-                            <span>Point</span></span>
+                            (
+                            <span class="text-[#B221E5]">300</span>
+                            <span>Point</span>
+                            )
+                        </span>
                     </h2>
-                    <p class="text-sm md:text-lg text-white mt-2">
+                    <p class="text-sm md:text-lg text-white mt-2 ">
                         {{ $quiz->question }}
                     </p>
                     {{-- // Soal quiz --}}
@@ -31,17 +34,17 @@
                         <div
                             class="flex flex-col md:flex-row mt-[24px] md:mt-[46px] mx-auto gap-[24px] md:gap-[74px] pl-0">
                             <button wire:click="setAnswer('A')"
-                                class="w-[260px] md:w-[315px] h-[52px] md:h-[63px] {{ isset($answers[$currentQuiz]) ? ($answers[$currentQuiz] == 'A' ? 'bg-pink-300' : 'bg-[#2F2F2F]') : 'bg-[#2F2F2F]' }}  rounded-lg">
+                                class="w-fit px-4 md:w-[315px] font-sm py-4 {{ isset($answers[$currentQuiz]) ? ($answers[$currentQuiz] == 'A' ? 'bg-pink-300 shadow-md shadow-pink-300' : 'bg-[#2F2F2F]') : 'bg-[#2F2F2F]' }}  rounded-lg transition duration-300 hover:-translate-y-2">
                                 <!-- A.  Lorem ipsum -->
-                                <div class="text-white text-[20px]">
+                                <div class="text-white">
                                     A. {{ $quiz->opt_a }}
                                 </div>
                                 <!-- A.  Lorem ipsum -->
                             </button>
                             <button wire:click="setAnswer('B')"
-                                class="w-[260px] md:w-[315px] h-[52px] md:h-[63px]  {{ isset($answers[$currentQuiz]) ? ($answers[$currentQuiz] == 'B' ? 'bg-pink-300' : 'bg-[#2F2F2F]') : 'bg-[#2F2F2F]' }} rounded-lg ">
+                                class="w-fit px-4 md:w-[315px] font-sm py-4  {{ isset($answers[$currentQuiz]) ? ($answers[$currentQuiz] == 'B' ? 'bg-pink-300 shadow-md shadow-pink-300' : 'bg-[#2F2F2F]') : 'bg-[#2F2F2F]' }} rounded-lg transition duration-300 hover:-translate-y-2">
                                 <!-- B.  Lorem ipsum -->
-                                <div class="text-white text-[20px]">
+                                <div class="text-white">
                                     B. {{ $quiz->opt_b }}
                                 </div>
                                 <!-- B.  Lorem ipsum -->
@@ -53,17 +56,17 @@
                         <div
                             class="flex flex-col md:flex-row mt-[24px] md:mt-[46px] mx-auto gap-[24px] md:gap-[74px] pl-0">
                             <button wire:click="setAnswer('C')"
-                                class="w-[260px] md:w-[315px] h-[52px] md:h-[63px] {{ isset($answers[$currentQuiz]) ? ($answers[$currentQuiz] == 'C' ? 'bg-pink-300' : 'bg-[#2F2F2F]') : 'bg-[#2F2F2F]' }} rounded-lg ">
+                                class="w-fit px-4 md:w-[315px] font-sm py-4  {{ isset($answers[$currentQuiz]) ? ($answers[$currentQuiz] == 'C' ? 'bg-pink-300 shadow-md shadow-pink-300' : 'bg-[#2F2F2F]') : 'bg-[#2F2F2F]' }} rounded-lg transition duration-300 hover:-translate-y-2">
                                 <!-- B.  Lorem ipsum -->
-                                <div class="text-white text-[20px]">
+                                <div class="text-white">
                                     C. {{ $quiz->opt_c }}
                                 </div>
                                 <!-- B.  Lorem ipsum -->
                             </button>
                             <button wire:click="setAnswer('D')"
-                                class="w-[260px] md:w-[315px] h-[52px] md:h-[63px {{ isset($answers[$currentQuiz]) ? ($answers[$currentQuiz] == 'D' ? 'bg-pink-300' : 'bg-[#2F2F2F]') : 'bg-[#2F2F2F]' }} rounded-lg ">
+                                class="w-fit px-4 md:w-[315px] font-sm py-4   {{ isset($answers[$currentQuiz]) ? ($answers[$currentQuiz] == 'D' ? 'bg-pink-300 shadow-md shadow-pink-300' : 'bg-[#2F2F2F]') : 'bg-[#2F2F2F]' }} rounded-lg transition duration-300 hover:-translate-y-2">
                                 <!-- B.  Lorem ipsum -->
-                                <div class="text-white text-[20px]">
+                                <div class="text-white">
                                     D. {{ $quiz->opt_d }}
                                 </div>
                                 <!-- B.  Lorem ipsum -->
@@ -76,9 +79,9 @@
                         {{-- Tof --}}
                         <div class="flex flex-row justify-center mt-10 mx-auto gap-8 md:gap-14">
                             <button wire:click="setAnswer('True')"
-                                class="bg-[#19AC66] {{ isset($answers[$currentQuiz]) ? ($answers[$currentQuiz] == 'True' ? 'shadow-lg shadow-indigo-500/50' : '') : '' }} w-[109px] md:w-[171px] h-[37px] md:h-[58px] rounded-lg text-white text-sm md:text-xl">True</button>
+                                class="bg-[#19AC66] {{ isset($answers[$currentQuiz]) ? ($answers[$currentQuiz] == 'True' ? 'shadow-lg shadow-[#19AC66]' : 'bg-[#2F2F2F]') : 'bg-[#2F2F2F]' }} w-[109px] md:w-[171px] h-[37px] md:h-[58px] rounded-lg text-white text-sm md:text-xl transition duration-300 hover:-translate-y-2">True</button>
                             <button wire:click="setAnswer('False')"
-                                class="bg-[#DE4343]  {{ isset($answers[$currentQuiz]) ? ($answers[$currentQuiz] == 'False' ? 'shadow-lg shadow-indigo-500/50' : '') : '' }} w-[109px] md:w-[171px] h-[37px] md:h-[58px] rounded-lg text-white text-sm md:text-xl">False</button>
+                                class="bg-[#DE4343]  {{ isset($answers[$currentQuiz]) ? ($answers[$currentQuiz] == 'False' ? 'shadow-lg shadow-[#DE4343]' : '') : '' }} w-[109px] md:w-[171px] h-[37px] md:h-[58px] rounded-lg text-white text-sm md:text-xl transition duration-300 hover:-translate-y-2">False</button>
                         </div>
                         {{-- End of Tof --}}
                     @endif
@@ -93,7 +96,7 @@
                                         class="absolute w-[177px] h-[159px] blur-lg opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-300 bg-gradient-to-r from-[#B221E5] to-[#5B1BE1] animate-tilt">
                                     </div>
                                     <div
-                                        class="relative w-[177px] h-[159px] bg-[#B5B3BC] text-white text-[20px] text-center pt-[56px] rounded-lg">
+                                        class="relative w-[177px] h-[159px] bg-[#B5B3BC] text-white text-center pt-[56px] rounded-lg">
                                         <img src="{{ $quiz->opt_a }}" alt="">
                                     </div>
                                     <div
@@ -111,7 +114,7 @@
                             <div class="flex flex-col mt-[46px] md:mt-0">
                                 <div class="w-[177px] h-[159px] bg-[#B5B3BC] rounded-lg ">
                                     <!-- pic2 -->
-                                    <div class="text-white text-[20px] text-center pt-[56px]">
+                                    <div class="text-white text-center pt-[56px]">
                                         <img src="{{ $quiz->opt_b }}" alt="">
                                     </div>
                                     <!-- pic2 -->
@@ -150,7 +153,34 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
                             </svg>
                         </button>
+                        @if (Auth::check())
+                            @if ($quizStatus[$currentQuiz]['is_done'])
+                                <p class=" text-white">Anda Sudah Menjawab Quiz ini dan bisa melihat pembahasan di
+                                    bawah.</p>
+                            @else
+                                <button
+                                    wire:click="submitAnswer"class="mx-auto text-white md:text-xl rounded-lg col-span-2 btn px-8 md:px-10 py-2 mt-8 md:mt-100 font-semibold">Submit</button>
+                            @endif
+                        @else
+                            <a class="mx-auto text-white md:text-xl rounded-lg col-span-2 btn px-8 md:px-10 py-2 mt-8 md:mt-100 font-semibold"
+                                href="{{ route('login') }}">Login</a>
+                        @endif
                     </div>
+                    @if (Auth::check() && $quizStatus[$currentQuiz]['is_done'])
+                        <!-- start: pembahasan-->
+                        <div class="divide-y-[1px] mt-[55px] divide-white">
+                            <h1 class="text-white"></h1>
+                            <h1 class="text-white pt-[29px] text-[14px] md:text-[20px] ">Jawaban yang benar:
+                                <br><span
+                                    class="text-[#B5B3BC] font-bold text-[18px] md:text-[24px]">{{ $quizzes[$currentQuiz]->answer }}</span>
+                                <br><span class="text-white text-[14px] md:text-[20px] ">Pembahasan :</span>
+                                <br><span class="text-[#B5B3BC] text-[14px] md:text-[20px]">
+                                    {!! $quizzes[$currentQuiz]->explanation !!}
+                                </span>
+                            </h1>
+                        </div>
+                        <!-- end: pembahasan-->
+                    @endif
                 </div>
 
                 {{-- Right Part --}}
@@ -161,56 +191,13 @@
                                 class="w-[56px] md:w-[75px] h-[56px] md:h-[75px] {{ $currentQuiz == $quizNo ? 'bg-gradient-to-r from-[#B221E5] to-[#5B1BE1] text-white' : ($quizStatus[$quizNo]['is_done'] ? ($quizStatus[$quizNo]['status'] ? 'bg-green-500 text-white' : 'bg-red-500 text-white') : 'bg-[#2F2F2F] text-white') }} mx-auto  text-2xl font-bold rounded-lg">{{ $quizNo + 1 }}</button>
                         @endforeach
                     </div>
-
-                    @if (Auth::check())
-                        @if ($quizStatus[$currentQuiz]['is_done'])
-                            <p class=" text-white">Anda Sudah Menjawab Quiz ini.</p>
-                        @else
-                            <button
-                                wire:click="submitAnswer"class="mx-auto text-white md:text-xl rounded-lg col-span-2 btn px-8 md:px-10 py-2 mt-8 md:mt-100 font-semibold">Submit</button>
-                        @endif
-                    @else
-                        <a class="mx-auto text-white md:text-xl rounded-lg col-span-2 btn px-8 md:px-10 py-2 mt-8 md:mt-100 font-semibold"
-                            href="{{ route('login') }}">Login</a>
                 </div>
-    @endif
-
-    <div class="flex flex-row gap-7 mx-auto mt-12 md:hidden">
-        <button class="bg-[#2F2F2F] w-[35px] h-[35px] text-white items-center rounded-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-auto" fill="none" viewBox="0 0 24 24"
-                stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
-            </svg>
-        </button>
-        <button class="bg-[#2F2F2F] w-[35px] h-[35px] text-white items-center rounded-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-auto" fill="none" viewBox="0 0 24 24"
-                stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
-            </svg>
-        </button>
-    </div>
-</div>
+            </div>
 
 
-</div>
 
-@if (Auth::check() && $quizStatus[$currentQuiz]['is_done'])
-    <!-- start: pembahasan-->
-    <div class="divide-y-[1px] mt-[55px] divide-white">
-        <h1 class="text-white"></h1>
-        <h1 class="text-white pt-[29px] text-[14px] md:text-[20px] ">Jawaban :
-            <br><span class="text-[#B5B3BC] font-bold text-[18px] md:text-[24px]">{{ $quizzes[$currentQuiz]->answer }}</span>
-            <br><span class="text-white text-[14px] md:text-[20px] ">Pembahasan :</span>
-            <br><span class="text-[#B5B3BC] text-[14px] md:text-[20px]">
-                {!! $quizzes[$currentQuiz]->explanation !!}
-            </span>
-        </h1>
-    </div>
-    <!-- end: pembahasan-->
-@endif
-
-</div>
-@endforeach
+        </div>
+    @endforeach
 </div>
 
 @push('js')
