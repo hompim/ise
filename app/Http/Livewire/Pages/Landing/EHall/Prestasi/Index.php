d33dbb64189bd7c7efdae155f59152160ca767c9
Cizz22 <cisatraa@gmail.com>
2022-09-18 15:13:35 +0700

    artikel prestasi

--- app/Http/Livewire/Pages/Landing/EHall/Prestasi/Index.php
+++ app/Http/Livewire/Pages/Landing/EHall/Prestasi/Index.php
@@ -21,8 +21,8 @@ class Index extends Component
         $count =  EhallArticle::count();
         $this->nextThree = EhallArticle::where('type', 'Prestasi')->take(3)->get();
 
-        for ($i = 0; $i < floor($count / 3); $i++) {
-            if ($i != 0) $this->nextThree = EhallArticle::where('type', 'Prestasi')->skip(3*$i)->take(3)->get();
+        for ($i = 1; $i < floor($count / 3); $i++) {
+            if ($i != 1) $this->nextThree = EhallArticle::where('type', 'Prestasi')->skip(3*$i)->take(3)->get();
             array_push(
                 $this->articles,
                 $this->nextThree
