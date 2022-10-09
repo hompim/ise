8e7d48f4b229db5665ce418a8eae92a07dd68c0f
MrWaradana <muhammadridhowaradana@gmail.com>
2022-10-09 12:38:45 +0700

    feat: modal trivia

---
+++ app/Http/Livewire/Components/ModalTrivia.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components;
+
+use Livewire\Component;
+
+class ModalTrivia extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.modal-trivia');
+    }
+}
