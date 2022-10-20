5e64be2bb78c4d1d0d9249ec77aa4db08d29e11b
MrWaradana <muhammadridhowaradana@gmail.com>
2022-10-20 15:39:56 +0700

    fix; submission bcl 2

--- resources/views/livewire/pages/bionix/peserta/tugas/task-card.blade.php
+++ resources/views/livewire/pages/bionix/peserta/tugas/task-card.blade.php
@@ -37,7 +37,8 @@
                             </a>
                         @endif
                     @else
-                        @if (!Auth::user()->userable->bionix->submission || !Auth::user()->userable->bionix->submission->video_link)
+                        @if (!Auth::user()->userable->bionix->submission->team_id ||
+                            !Auth::user()->userable->bionix->submission->video_link)
                             <input type="text" class="text-black w-100" wire:model.defer="linkTask"
                                 class="form-control-file" name="linkTask" id="linkTask"
                                 placeholder="Masukkan link video">
